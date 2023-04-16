70fbb01e5f91f86b2f66afc548e1da9053088074
benlin1211 <ben1211.eecs06@nctu.edu.tw>
2023-04-16 15:29:46 +0000

    delete trainer, add bash file

---
+++ hw1_download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download dataset
+
+# download checkpoint
+
+# unzip checkpoint to correct directory
+
+# inference
+python ./yolov8/main.py --resume ./yolov8/runs/detect/train/weights/best.pt --eval
